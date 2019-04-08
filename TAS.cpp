c6f5d898bf373f5ab73c974ddbc9d46af3b63ecc
anurag <anuraagsinghk1998@gmail.com>
2019-04-08 19:05:10 +0530

    modify TAS.cpp: add time

--- TAS.cpp
+++ TAS.cpp
@@ -2,8 +2,13 @@
 #include <thread>
 #include <atomic>
 #include <unistd.h>
+#include <mutex>
+#include <sstream>
+#include <fstream>
 
 using namespace std;
+
+mutex mtx;
 
 class TASLock{
 	atomic<bool> state;
@@ -24,32 +29,77 @@ class TASLock{
 
 void testCS(int id, int k, TASLock &testLock){
 	for(int i=0; i<k; i++){
-		// get the time
-		// cout<<"thread "<<id<<" request the cs\n";
+		// ---------- get the time
+		time_t rawtime;
+		time(&rawtime);
+		struct tm *timeinfo = localtime(&rawtime);
+
+		
+		
+		mtx.lock();
+		cout<<i+1<<" CS requested by thread "<<id<<" at "<<asctime(timeinfo);
+		mtx.unlock();
+
 		testLock.lock();
-		cout<<"thread "<<id<<" enter the cs\n";
-		sleep(1);
+		mtx.lock();
+		cout<<i+1<<" CS Entered by thread "<<id<<" at "<<asctime(timeinfo);
+		mtx.unlock();
+		sleep(1); // ------------change sleep to exp dist
 		testLock.unlock();
-		// cout<<"thread "<<id<<"exit the cs\n";
+		
+		mtx.lock();
+		cout<<i+1<<" CS Exited by thread "<<id<<" at "<<asctime(timeinfo);
+		mtx.unlock();
+			
 	}
 	return;
 }
 
-int main(){
-	int n=5;
-	int k=5;
+int main(int argc, char const *argv[1]){
+	
+	int N, k, lmb1, lmb2;
 
+	// read input from the file
+	ifstream inFile;
+	string filename = argv[1];
+	inFile.open(filename);
+    if(!inFile){
+        cout<<"unable to openfile"<<endl;
+        exit(1);
+    }
+
+    string line;
+    while(getline(inFile, line)){
+         istringstream iss(line);
+
+         int x;
+         // get the distribution logparameters
+		 // n k lmb1 lmb2
+         int cn = 0;
+         while((iss>>x)){
+             if(cn == 0)
+                 N = x;
+             else if(cn == 1)
+                 k = x;
+			 else if(cn == 2)
+				lmb1 = x;
+			 else if(cn == 3)
+				lmb2 = x;
+             cn++;
+         }
+    }
 
 	TASLock testLock;
-	thread thrdArr[n];
-	for(int i=0; i<n; i++){
+	thread thrdArr[N];
+	for(int i=0; i<N; i++){
 		thrdArr[i] = thread(testCS, i, k, ref(testLock));
 	}
 	
-	for(int i=0; i<n; i++){
+	for(int i=0; i<N; i++){
 		thrdArr[i].join();
 	}
 
+	inFile.close();
 	return 1;
 }
 
